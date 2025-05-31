3d4d58019e77bee371c4c7dd165c90f44d6c8823
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-31 20:33:18 -0300

    feat: implement useHasMounted hook and optimize sidebar state management

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,8 +1,14 @@
+"use client"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { useHasMounted } from "@/hooks/use-has-mounted";
 
 import { AppSidebar } from "./_components/app-sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const hasMounted = useHasMounted()
+
+  if (!hasMounted) return null
+
   return (
     <SidebarProvider>
       <AppSidebar />
